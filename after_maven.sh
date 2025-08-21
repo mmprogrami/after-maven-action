2e85b2201d8f7b2566c6ca18a27a1aeef43d542a
Michiel Meeuwissen <michiel meeuwissen>
2025-08-21 12:20:01 +0200

    details.

--- after_maven.sh
+++ after_maven.sh
@@ -44,10 +44,16 @@ fi
 cat ${JOB_ENV}
 source ${JOB_ENV}
 
-if [ $MAVEN_TESTS_ERROR gt 0 ] ; then
+if [ $MAVEN_TESTS_ERROR -gt 0 ] ; then
+  echo  "Some ($MAVEN_TESTS_ERROR) tests had errors. Exit 1." 1>&2
   exit 1
 elif [ $MAVEN_TESTS_FAILED -gt 0 ]; then
+  echo  "Some ($MAVEN_TESTS_ERROR) tests had failures. Exit 2" 1>&2
   exit 2
+elif [ $MAVEN_TESTS_RUN -eq 0 ]; then
+  echo  "Everything seems ok, but now tests run. Exit 0" 1>&2
+  exit 0
 else
+  echo  "All tests passed. Exit 0"
   exit 0
 fi
